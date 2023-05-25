5e69c9a98b94da25e53335d7b1855eea1b52f1de
cjparsonson <cjparsonson@gmail.com>
2023-05-25 17:08:14 +0100

    Add project files.

---
+++ NetworkInfo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+String hostname = Dns.GetHostName();
+IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+WriteLine($"Hostname: {hostname}");
+foreach (IPAddress address in addresses)
+{
+    switch (address.AddressFamily)
+    {
+        case System.Net.Sockets.AddressFamily.InterNetwork:
+            WriteLine($"IPv4: {address}");
+            break;
+        case System.Net.Sockets.AddressFamily.InterNetworkV6:
+            WriteLine($"IPv6: {address}");
+            break;
+        default:
+            WriteLine($"Unknown: {address}");
+            break;
+            
+    }
+    
+}
+
+
