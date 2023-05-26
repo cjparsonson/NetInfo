cadc3a607861a8969aedca36b459326f07c3bc45
cjparsonson <cjparsonson@gmail.com>
2023-05-26 16:15:24 +0100

    Updates

--- NetworkInfo/Program.cs
+++ NetworkInfo/Program.cs
@@ -1,14 +1,26 @@
-﻿using System.Net;
+﻿using System.Data.Common;
+using System.Net;
+using System.Net.NetworkInformation;
 
 String hostname = Dns.GetHostName();
 IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+
 WriteLine($"Hostname: {hostname}");
+foreach (NetworkInterface adapter in interfaces)
+{
+    IPv4InterfaceProperties properties = adapter.IPv4InterfaceProperties();
+    WriteLine($"{ adapter.Description}: {properties.}");
+}
+WriteLine();
 foreach (IPAddress address in addresses)
 {
     switch (address.AddressFamily)
     {
         case System.Net.Sockets.AddressFamily.InterNetwork:
             WriteLine($"IPv4: {address}");
+            WriteLine(address);
             break;
         case System.Net.Sockets.AddressFamily.InterNetworkV6:
             WriteLine($"IPv6: {address}");
@@ -20,5 +32,10 @@ foreach (IPAddress address in addresses)
     }
     
 }
+WriteLine();
+
+
+
+
 
 
